b66f641c610c9bd33a7ffebe39ee16653180e2ca
Martin Tan <mtanpub@gmail.com>
2026-05-07 21:51:46 +0800

    update participable

--- app/models/concerns/participable.rb
+++ app/models/concerns/participable.rb
@@ -12,6 +12,32 @@
 module Participable
   extend ActiveSupport::Concern
 
+  class_methods do
+    # Adds a list of participant attributes. Attributes can either be symbols or
+    # Procs.
+    #
+    # When using a Proc instead of a Symbol the Proc will be given two
+    # arguments:
+    #
+    # 1. The current user (as an instance of User)
+    # 2. An instance of `Gitlab::ReferenceExtractor`
+    #
+    # It is expected that a Proc populates the given reference extractor
+    # instance with data. The return value of the Proc is ignored.
+    #
+    # attr - The name of the attribute or a Proc
+    def participant(attr)
+      participant_attrs << attr
+    end
+  end
+
+  included do
+    # Accessor for participant attributes.
+    cattr_accessor :participant_attrs, instance_accessor: false do
+      []
+    end
+  end
+
   def participants(_user = nil)
     result = []
     result << author if respond_to?(:author)
