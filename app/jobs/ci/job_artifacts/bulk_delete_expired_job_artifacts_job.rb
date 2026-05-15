c4f264c62d51679e89bca3ed677f4d4f5e5ffecc
Martin Tan <mtanpub@gmail.com>
2026-05-15 15:04:43 +0800

    add cron delete_expired_job_artifacts

---
+++ app/jobs/ci/job_artifacts/bulk_delete_expired_job_artifacts_job.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# ======================================================
+# Contains code from GitLab FOSS (MIT Licensed)
+# Copyright (c) GitLab Inc.
+# See .licenses/Gisia/others/gitlab-foss.dep.yml for full license
+#
+# Modifications and additions copyright (c) 2025-present Liuming Tan
+# Licensed under AGPLv3 - see LICENSE file in this repository
+# ======================================================
+
+module Ci
+  module JobArtifacts
+    class BulkDeleteExpiredJobArtifactsJob < ApplicationJob
+      queue_as :default
+
+      def perform
+        Ci::JobArtifacts::DestroyAllExpiredService.new.execute
+      end
+    end
+  end
+end
