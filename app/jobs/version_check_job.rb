d28507d07fe4ef45e19d6e0d960e4ba001c756d5
Martin Tan <mtanpub@gmail.com>
2026-05-18 17:28:27 +0800

    update version check job

--- app/jobs/version_check_job.rb
+++ app/jobs/version_check_job.rb
@@ -7,7 +7,7 @@ class VersionCheckJob < ApplicationJob
     return unless Rails.env.production?
     return unless ApplicationSetting.current.version_check_enabled
 
-    encoded = Base64.urlsafe_encode64({ version: Gitlab::VERSION }.to_json)
+    encoded = Base64.urlsafe_encode64({ version: Gisia::VERSION }.to_json)
     uri = URI("https://version.gisia.dev/check?encoded_version=#{encoded}")
     Net::HTTP.get(uri)
   end
