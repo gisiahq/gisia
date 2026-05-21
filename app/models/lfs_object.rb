c0f5eae5b50aa1e57c2a382f61322c1e35840e5b
Martin Tan <mtanpub@gmail.com>
2026-05-21 23:12:49 +0800

    fix lfs pull

--- app/models/lfs_object.rb
+++ app/models/lfs_object.rb
@@ -47,13 +47,14 @@ class LfsObject < ApplicationRecord
   end
 
   def project_allowed_access?(project)
-    if project.fork_network_member
-      lfs_objects_projects
-        .where("EXISTS(?)", project.fork_network.fork_network_members.select(1).where("fork_network_members.project_id = lfs_objects_projects.project_id"))
-        .exists?
-    else
-      lfs_objects_projects.where(project_id: project.id).exists?
-    end
+    # TODO: implement fork network access check when fork support is added
+    # if project.fork_network_member
+    #   lfs_objects_projects
+    #     .where("EXISTS(?)", project.fork_network.fork_network_members.select(1).where("fork_network_members.project_id = lfs_objects_projects.project_id"))
+    #     .exists?
+    # else
+    lfs_objects_projects.where(project_id: project.id).exists?
+    # end
   end
 
   def local_store?
