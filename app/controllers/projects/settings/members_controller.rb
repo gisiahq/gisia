d14a952baaa42fd21f0b0f1b09ee5293fdecfd06
Martin Tan <mtanpub@gmail.com>
2026-06-12 22:04:57 +0800

    fix member role update permission check

--- app/controllers/projects/settings/members_controller.rb
+++ app/controllers/projects/settings/members_controller.rb
@@ -3,6 +3,8 @@ module Projects
     class MembersController < Projects::Settings::ApplicationController
       include MembersHelper
       before_action :set_member, only: [:edit_form, :update, :destroy]
+      before_action :authorize_member_role!, only: [:create, :update, :destroy]
+      before_action :prevent_last_owner_removal!, only: [:destroy]
 
       def index
         @members = @project.namespace.members.includes(:user).non_request.order(access_level: :desc, id: :asc)
@@ -72,6 +74,36 @@ module Projects
 
       def set_member
         @member = @project.namespace.members.find(params[:id])
+      end
+
+      def authorize_member_role!
+        return if current_user&.admin?
+        return head :forbidden if @member && !can_assign_level?(@member.access_level)
+
+        requested_level = member_param_present? ? member_params[:access_level] : nil
+        head :forbidden if requested_level.present? && !can_assign_level?(requested_level)
+      end
+
+      def prevent_last_owner_removal!
+        return unless @member.owner?
+        return if @project.namespace.members.owners.where.not(id: @member.id).exists?
+
+        redirect_to settings_members_path(@project), alert: 'Cannot remove the last owner of the project.'
+      end
+
+      def member_param_present?
+        params.key?(:project_member) || params.key?(:member)
+      end
+
+      def can_assign_level?(level)
+        Gitlab::Access.level_encompasses?(
+          current_access_level: actor_access_level,
+          level_to_assign: Member.access_levels.fetch(level.to_s, level).to_i
+        )
+      end
+
+      def actor_access_level
+        @actor_access_level ||= @project.team.max_member_access(current_user.id)
       end
 
       def member_params
