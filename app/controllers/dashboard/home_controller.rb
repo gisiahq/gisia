654774f1d0fa3be44ef00393123150f86964860e
Martin Tan <mtanpub@gmail.com>
2026-07-05 23:25:30 +0800

    show authrized projects on homepage

--- app/controllers/dashboard/home_controller.rb
+++ app/controllers/dashboard/home_controller.rb
@@ -1,7 +1,7 @@
 class Dashboard::HomeController < Dashboard::ApplicationController
   def home
-    if current_user.projects.exists?
-      @projects = order_pinned_first(current_user.projects).page(params[:page]).per(20)
+    if current_user.authorized_projects.exists?
+      @projects = order_pinned_first(current_user.authorized_projects).page(params[:page]).per(20)
       render 'dashboard/projects/index'
     else
       @username = current_user.name
