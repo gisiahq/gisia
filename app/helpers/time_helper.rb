953df76be6aa351e60d5cb2095493db3d6b83f95
Martin Tan <mtanpub@gmail.com>
2026-07-14 15:38:30 +0800

    show durations on pipeline pages

---
+++ app/helpers/time_helper.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# ======================================================
+# Contains code from GitLab FOSS (MIT Licensed)
+# Copyright (c) GitLab Inc.
+# See .licenses/Gisia/others/gitlab-foss.dep.yml for full license
+# ======================================================
+
+module TimeHelper
+  TIME_UNIT_TRANSLATION = {
+    seconds: ->(seconds) { n_('%d second', '%d seconds', seconds) % seconds },
+    minutes: ->(minutes) { n_('%d minute', '%d minutes', minutes) % minutes },
+    hours: ->(hours) { n_('%d hour', '%d hours', hours) % hours },
+    days: ->(days) { n_('%d day', '%d days', days) % days },
+    weeks: ->(weeks) { n_('%d week', '%d weeks', weeks) % weeks },
+    months: ->(months) { n_('%d month', '%d months', months) % months },
+    years: ->(years) { n_('%d year', '%d years', years) % years }
+  }.freeze
+
+  def time_interval_in_words(interval_in_seconds)
+    time_parts = ActiveSupport::Duration.build(interval_in_seconds.to_i).parts
+
+    time_parts.map { |unit, value| TIME_UNIT_TRANSLATION[unit].call(value) }.to_sentence
+  end
+
+  def time_in_milliseconds
+    (Time.now.to_f * 1000).to_i
+  end
+end
